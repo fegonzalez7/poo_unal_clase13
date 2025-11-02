b8de47fa6bdbd472d7bf5ced80a032966328321a
Felipe Gonzalez <fegonzalez17@gmail.com>
2025-11-02 11:31:58 -0500

    chore style adjust

--- hello.py
+++ hello.py
@@ -1,19 +1,21 @@
 from collections import defaultdict
 
+
 def letter_frequency_normal(sentence):
-  frequencies = {}  # Diccionario normal
-  for letter in sentence:
-    frequency = frequencies.setdefault(letter, 0)  # Verifica si la clave existe
-    frequencies[letter] = frequency + 1
-  return frequencies
+    frequencies = {}  # Diccionario normal
+    for letter in sentence:
+        frequency = frequencies.setdefault(letter, 0)  # Verifica si la clave existe
+        frequencies[letter] = frequency + 1
+    return frequencies
 
 
 def letter_frequency_default(sentence):
-  frequencies = defaultdict(int)  # defaultdict con valor por defecto int(0)
-  for letter in sentence:
-    frequencies[letter] += 1
-  return frequencies # defaultdict se puede indexar igual que un diccionario normal
+    frequencies = defaultdict(int)  # defaultdict con valor por defecto int(0)
+    for letter in sentence:
+        frequencies[letter] += 1
+    return frequencies  # defaultdict se puede indexar igual que un diccionario normal
 
-if __name__ == '__main__':
-  print(letter_frequency_normal("hola mundo"))
-  print(letter_frequency_default("hola mundo")) 
+
+if __name__ == "__main__":
+    print(letter_frequency_normal("hola mundo"))
+    print(letter_frequency_default("hola mundo"))
